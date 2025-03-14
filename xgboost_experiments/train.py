0b3c5d4104f374a7e5772d03ffcb07359e3703a1
Perry <kperry@nrel.gov>
2025-03-14 16:52:01 -0600

    added tuning boolean on whether or not we want to tune xgb model

--- xgboost_experiments/train.py
+++ xgboost_experiments/train.py
@@ -14,6 +14,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 predict_train_data = True
+tune_model = False
 
 # Define the random search grid
 parameters = {'learning_rate': [0.001, 0.01, 0.05, 0.1],
@@ -138,66 +139,70 @@ if __name__ == '__main__':
     # variables
     X = time_series.drop(['value'], axis=1).values
     y = time_series['value'].values
-    # Now let's tune using random search
-    xgb_model = xgb.XGBRegressor()
-    random_search_model = RandomizedSearchCV(xgb_model,
-                                             parameters,
-                                             n_iter=20,
-                                             cv=3,
-                                             scoring='neg_mean_absolute_error',
-                                             verbose=5,
-                                             n_jobs=2)
-    random_search_model.fit(X, y, verbose=1)
-    tuned_hyperparameters = random_search_model.best_params_
-    # Take the best hyperparameters and rerun the model with the best parameters
-    xgb_model_optimized = xgb.XGBRegressor(**tuned_hyperparameters)
-    xgb_model_optimized.fit(X, y, verbose=1)
-    # Write the optimized model to memory
-    xgb_model_optimized.save_model("optimized_xgboost_model.json")
-    # Load in the test data
-    test_x = pd.read_csv("C:/Users/kperry/Documents/source/repos/smooth_multiperiodic_forecasting_experiments/X_out_sample.csv",
-                           parse_dates=True,
-                           index_col=0)
-    test_y = pd.read_csv("C:/Users/kperry/Documents/source/repos/smooth_multiperiodic_forecasting_experiments/Y_out_sample.csv",
-                          parse_dates=True,
-                          index_col=0)
-    # Clean up the data for the model
-    test_time_series = reformat_data_to_regression(test_x, test_y)
-    # Normalize with respect to the training data
-    for col in list(test_time_series):
-        if col in ['horizon_step_number', 'horizon_hour_sin', 'horizon_hour_cos',
-                   'horizon_day_sin', 'horizon_day_cos', '0', '1', '2', '3',
-                   '4', '5']:
-            pass
-        else:
-            data_vals = [x for x in normalization_params_list if x["col"] == col][0]
-            min_val, max_val = data_vals['min'], data_vals['max']
-            test_time_series[col] = min_max_normalize(min_val, max_val,
-                                                      test_time_series[col])
-    # Predict for test set
-    predict_X = test_time_series.drop(['value'], axis=1).values
-    real_y = test_time_series['value'].values
-    # Run the test data through the model to generate predictions
-    predict_y = pd.Series(xgb_model_optimized.predict(predict_X))
-    # Un-normalize the data w/ respect to the training data
-    target_vals = [x for x in normalization_params_list if x["col"] == "value"][0]
-    predict_y_unnormalized = ((predict_y * (target_vals['max'] -
-                                           target_vals['min'])) +
-                              target_vals['min'])
-    predict_y_unnormalized.index=test_time_series.index
-    real_y_unnormalized = ((real_y * (target_vals['max'] -
-                                           target_vals['min'])) +
-                              target_vals['min'])
-    # Take the results and pivot them so that they're in the original 
-    # expected format
-    residuals = real_y_unnormalized - predict_y_unnormalized
-    print("RESIDUALS RESULTS:")
-    print("MAE:" + str(abs(residuals).mean()))
-    print("Median ABS ERROR:" + str(abs(residuals).median()))    
-    results_df = pd.DataFrame({"datetime": test_time_series.index,
-                                'horizon_step_number': test_time_series['horizon_step_number'],
-                               "predicted": predict_y_unnormalized})
-    # Pivot the dataframe back 
-    results_df_pivoted = results_df.pivot(index="datetime",
-                                          columns="horizon_step_number")['predicted']
-    results_df_pivoted.to_csv("xgboost_results.csv")
+    if tune_model:
+        # Now let's tune using random search
+        xgb_model = xgb.XGBRegressor()
+        random_search_model = RandomizedSearchCV(xgb_model,
+                                                 parameters,
+                                                 n_iter=20,
+                                                 cv=3,
+                                                 scoring='neg_mean_absolute_error',
+                                                 verbose=5,
+                                                 n_jobs=2)
+        random_search_model.fit(X, y, verbose=1)
+        tuned_hyperparameters = random_search_model.best_params_
+        # Take the best hyperparameters and rerun the model with the best parameters
+        xgb_model_optimized = xgb.XGBRegressor(**tuned_hyperparameters)
+        xgb_model_optimized.fit(X, y, verbose=1)
+        # Write the optimized model to memory
+        xgb_model_optimized.save_model("optimized_xgboost_model.json")
+    else:
+        xgb_model_optimized = xgb.XGBRegressor() # or which ever sklearn booster you're are using
+        xgb_model_optimized.load_model("optimized_xgboost_model.json")
+        # Load in the test data
+        test_x = pd.read_csv("C:/Users/kperry/Documents/source/repos/smooth_multiperiodic_forecasting_experiments/X_out_sample.csv",
+                               parse_dates=True,
+                               index_col=0)
+        test_y = pd.read_csv("C:/Users/kperry/Documents/source/repos/smooth_multiperiodic_forecasting_experiments/Y_out_sample.csv",
+                              parse_dates=True,
+                              index_col=0)
+        # Clean up the data for the model
+        test_time_series = reformat_data_to_regression(test_x, test_y)
+        # Normalize with respect to the training data
+        for col in list(test_time_series):
+            if col in ['horizon_step_number', 'horizon_hour_sin', 'horizon_hour_cos',
+                       'horizon_day_sin', 'horizon_day_cos', '0', '1', '2', '3',
+                       '4', '5']:
+                pass
+            else:
+                data_vals = [x for x in normalization_params_list if x["col"] == col][0]
+                min_val, max_val = data_vals['min'], data_vals['max']
+                test_time_series[col] = min_max_normalize(min_val, max_val,
+                                                          test_time_series[col])
+        # Predict for test set
+        predict_X = test_time_series.drop(['value'], axis=1).values
+        real_y = test_time_series['value'].values
+        # Run the test data through the model to generate predictions
+        predict_y = pd.Series(xgb_model_optimized.predict(predict_X))
+        # Un-normalize the data w/ respect to the training data
+        target_vals = [x for x in normalization_params_list if x["col"] == "value"][0]
+        predict_y_unnormalized = ((predict_y * (target_vals['max'] -
+                                               target_vals['min'])) +
+                                  target_vals['min'])
+        predict_y_unnormalized.index=test_time_series.index
+        real_y_unnormalized = ((real_y * (target_vals['max'] -
+                                               target_vals['min'])) +
+                                  target_vals['min'])
+        # Take the results and pivot them so that they're in the original 
+        # expected format
+        residuals = real_y_unnormalized - predict_y_unnormalized
+        print("RESIDUALS RESULTS:")
+        print("MAE:" + str(abs(residuals).mean()))
+        print("Median ABS ERROR:" + str(abs(residuals).median()))    
+        results_df = pd.DataFrame({"datetime": test_time_series.index,
+                                    'horizon_step_number': test_time_series['horizon_step_number'],
+                                   "predicted": predict_y_unnormalized})
+        # Pivot the dataframe back 
+        results_df_pivoted = results_df.pivot(index="datetime",
+                                              columns="horizon_step_number")['predicted']
+        results_df_pivoted.to_csv("xgboost_results.csv")
